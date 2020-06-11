82fc426eb254f42c26c85ad942513c74f3c8b353
kevinsbstn4 <kevincaesar22@gmail.com>
2020-06-11 17:00:07 +0700

    get client body and change response status

--- falcon/basic-rest-api/app.py
+++ falcon/basic-rest-api/app.py
@@ -2,13 +2,28 @@ import json, falcon
 
 class ObjRequestClass:
     def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        data = json.loads(req.stream.read())
+
         content = {
            'name' : 'kevin',
            'kelas' : '2',
            'asal' : 'semarang'
         }
 
-        resp.body = json.dumps(content)
+
+        output = {}
+        if('method' not in data):
+           resp.status = falcon.HTTP_501
+           output['value'] = 'tidak ada method'
+        else:
+            if(data['method'] == 'get-name'): 
+                output['value'] = content['name']
+            else: 
+                resp.status = falcon.HTTP_404
+                output['value'] = None
+         
+        resp.body = json.dumps(output)
 
 
 api = falcon.API()
