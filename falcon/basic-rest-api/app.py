324e77603c8281ae237814953404c54aa11c0afb
kevinsbstn4 <kevincaesar22@gmail.com>
2020-06-13 15:14:55 +0700

    get,post

--- falcon/basic-rest-api/app.py
+++ falcon/basic-rest-api/app.py
@@ -5,26 +5,48 @@ class ObjRequestClass:
         resp.status = falcon.HTTP_200
         data = json.loads(req.stream.read())
 
-        content = {
-           'name' : 'kevin',
-           'kelas' : '2',
-           'asal' : 'semarang'
+        # content = {
+        #    'name' : 'kevin',
+        #    'kelas' : '2',
+        #    'asal' : 'semarang'
+        # }
+
+
+        output = {
+            'msg' : 'Hello {0}'.format(data['method'])
         }
 
-
-        output = {}
-        if('method' not in data):
-           resp.status = falcon.HTTP_501
-           output['value'] = 'tidak ada method'
-        else:
-            if(data['method'] == 'get-name'): 
-                output['value'] = content['name']
-            else: 
-                resp.status = falcon.HTTP_404
-                output['value'] = None
-         
         resp.body = json.dumps(output)
 
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+        data = json.loads(req.stream.read())
+
+        equal = int(data['x']) + int(data['y']) 
+
+        output = {
+            'msg' : 'x: {x} + y: {y} is equal to {e}'.format(x=data['x'],y=data['y'],e=equal) 
+        }
+
+        resp.body = json.dumps(output)
+    def on_put(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+        output = {
+            'msg' : 'tidak terdapat put disini :('
+        }
+
+        resp.body = json.dumps(output)
+
+    def on_delete(self, req, resp):
+        resp.status = falcon.HTTP_200
+
+        output = {
+            'msg' : 'tidak terdapat delete disini'
+        }
+
+        resp.body = json.dumps(output)
 
 api = falcon.API()
 api.add_route('/test', ObjRequestClass())
